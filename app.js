03e525ffd5a6e2ce9111ebee8d8e78673581c28f
karlaaraujo <karlachs.a@gmail.com>
2021-07-10 10:47:11 -0300

    fixes

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", function (req,res) {
     res.sendFile(__dirname + "/index.html");
 })
 
-app.listen (3000, function(){
+app.listen (process.ev.PORT || 3000, function(){
     console.log("Running on port 3000");
 })
